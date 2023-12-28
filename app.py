bab7a30e9148e46dd0cf646a093559cb637e15a8
Casey Barajas <laughablelosers69@gmail.com>
2023-12-27 21:45:19 -0600

    fixed variable bug

--- app.py
+++ app.py
@@ -6,24 +6,24 @@ app = Flask(__name__)
 arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 arduino.flush()
 
-data = {"counter": 0}
+arduino_data = {"counter": 0}  # Renamed variable to avoid conflict
 
 def read_from_arduino():
     while True:
         if arduino.in_waiting > 0:
             line = arduino.readline().decode('utf-8').rstrip()
-            data["counter"] = line
+            arduino_data["counter"] = line  # Use the renamed variable
 
 thread = threading.Thread(target=read_from_arduino)
 thread.start()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=data)
+    return render_template('index.html', data=arduino_data)  # Pass the renamed variable
 
 @app.route('/data')
 def data():
-    return jsonify(data)
+    return jsonify(arduino_data)  # Return the renamed variable as JSON
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
